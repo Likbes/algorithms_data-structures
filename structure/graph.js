31fe342fbe978a59505254bdf5933133390c5719
Likbes <likbes.js@gmail.com>
2019-07-08 17:56:29 +0300

    create breadth first search on graph

--- structure/graph.js
+++ structure/graph.js
@@ -1,109 +1,137 @@
 class Graph {
-	constructor() {
-		this.adjacencyList = {};
-	}
+  constructor() {
+    this.adjacencyList = {};
+  }
 
-	addVertex(name) {
-		if (!this.adjacencyList[name]) this.adjacencyList[name] = [];
-		return this;
-	}
+  addVertex(name) {
+    if (!this.adjacencyList[name]) this.adjacencyList[name] = [];
+    return this;
+  }
 
-	addEdge(firstV, secondV) {
-		if (!this.adjacencyList[firstV] || !this.adjacencyList[secondV])
-			return undefined;
+  addEdge(firstV, secondV) {
+    if (!this.adjacencyList[firstV] || !this.adjacencyList[secondV])
+      return undefined;
 
-		this.adjacencyList[firstV].push(secondV);
-		this.adjacencyList[secondV].push(firstV);
-		return this;
-	}
+    this.adjacencyList[firstV].push(secondV);
+    this.adjacencyList[secondV].push(firstV);
+    return this;
+  }
 
-	removeEdge(firstVertex, secondVertex) {
-		const firstVertexArr = this.adjacencyList[firstVertex];
-		const secondVertexArr = this.adjacencyList[secondVertex];
+  removeEdge(firstVertex, secondVertex) {
+    const firstVertexArr = this.adjacencyList[firstVertex];
+    const secondVertexArr = this.adjacencyList[secondVertex];
 
-		if (!firstVertexArr || !secondVertexArr)
-			return undefined;
+    if (!firstVertexArr || !secondVertexArr)
+      return undefined;
 
-		this.adjacencyList[firstVertex] =
-			firstVertexArr.filter(vertex => vertex !== secondVertex);
+    this.adjacencyList[firstVertex] =
+      firstVertexArr.filter(vertex => vertex !== secondVertex);
 
-		this.adjacencyList[secondVertex] =
-			secondVertexArr.filter(vertex => vertex !== firstVertex);
+    this.adjacencyList[secondVertex] =
+      secondVertexArr.filter(vertex => vertex !== firstVertex);
 
-		return this;
-	}
+    return this;
+  }
 
-	removeVertex(name) {
-		const vertexArr = this.adjacencyList[name];
-		if (!vertexArr) return undefined;
+  removeVertex(name) {
+    const vertexArr = this.adjacencyList[name];
+    if (!vertexArr) return undefined;
 
-		vertexArr.forEach(edge => {
-			this.removeEdge(name, edge);
-		});
+    vertexArr.forEach(edge => {
+      this.removeEdge(name, edge);
+    });
 
-		delete this.adjacencyList[name];
-		return this;
-	}
+    delete this.adjacencyList[name];
+    return this;
+  }
 
-	DFSRecursively(start) {
-		const result = [];
-		const visitedVertex = [];
+  DFSRecursively(start) {
+    const result = [];
+    const visitedVertex = [];
 
-		const visitVertex = vertex => {
-			const currVertexArr = this.adjacencyList[vertex];
-			if (!currVertexArr.length) return null;
+    const visitVertex = vertex => {
+      const currVertexArr = this.adjacencyList[vertex];
+      if (!currVertexArr.length) return null;
 
-			result.push(vertex);
-			visitedVertex.push(vertex);
+      result.push(vertex);
+      visitedVertex.push(vertex);
 
-			currVertexArr.forEach(neighbor => {
-				if (!visitedVertex.includes(neighbor)) {
-					visitVertex(neighbor);
-				};
-			});
-		}
+      currVertexArr.forEach(neighbor => {
+        if (!visitedVertex.includes(neighbor)) {
+          visitVertex(neighbor);
+        };
+      });
+    }
 
-		visitVertex(start);
-		return result;
-	}
+    visitVertex(start);
+    return result;
+  }
 
-	DFSIteratively(start) {
-		let stack = [start];
-		let visitedVertex = [start];
-		let result = [start];
+  DFSIteratively(start) {
+    let stack = [start];
+    let visitedVertex = [start];
+    let result = [start];
 
-		while (stack.length > 0) {
-			const vertex = stack.pop();
+    while (stack.length > 0) {
+      const vertex = stack.pop();
 
-			this.adjacencyList[vertex].forEach(neighbor => {
-				if (!visitedVertex.includes(neighbor)) {
-					visitedVertex.push(neighbor);
-					result.push(neighbor);
-					stack.push(neighbor);
-				};
-			});
-		}
+      this.adjacencyList[vertex].forEach(neighbor => {
+        if (!visitedVertex.includes(neighbor)) {
+          visitedVertex.push(neighbor);
+          result.push(neighbor);
+          stack.push(neighbor);
+        };
+      });
+    }
 
-		return result;
-	}
+    return result;
+  }
+
+  BFS(start) {
+    let queue = [start];
+    let visitedVertex = [start];
+    let result = [start];
+
+    while (queue.length > 0) {
+      const vertex = queue.shift();
+
+      this.adjacencyList[vertex].forEach(neighbor => {
+        if (!visitedVertex.includes(neighbor)) {
+          visitedVertex.push(neighbor);
+          result.push(neighbor);
+          queue.push(neighbor);
+        };
+      });
+    }
+
+    return result;
+  }
 }
 
 const graph = new Graph();
 
 graph
-	.addVertex('Likbes')
-	.addVertex('Kirito')
-	.addVertex('Ugr')
-	.addVertex('Egor')
-	.addVertex('Vika')
-	.addVertex('Lena')
-	.addEdge('Likbes', 'Kirito')
-	.addEdge('Likbes', 'Egor')
-	.addEdge('Kirito', 'Ugr')
-	.addEdge('Egor', 'Lena')
-	.addVertex('Ugr', 'Lena')
-	.addEdge('Ugr', 'Vika')
-	.addEdge('Vika', 'Lena')
+  .addVertex('Likbes')
+  .addVertex('Kirito')
+  .addVertex('Ugr')
+  .addVertex('Egor')
+  .addVertex('Vika')
+  .addVertex('Lena')
+  .addEdge('Likbes', 'Kirito')
+  .addEdge('Likbes', 'Ugr')
+  .addEdge('Kirito', 'Lena')
+  .addEdge('Lena', 'Egor')
+  .addVertex('Lena', 'Vika')
+  .addEdge('Ugr', 'Egor')
+  .addEdge('Vika', 'Egor')
 
 console.log(graph.DFSRecursively('Likbes'));
-console.log(graph.DFSIteratively('Likbes'))
+console.log(graph.BFS('Likbes'))
+
+//							Likbes
+// 						/   		 \
+// 				 Kirito			 Ugr
+// 					 |					|
+// 					Lena - - - Egor	
+// 						 \		  /
+// 							 Vika
