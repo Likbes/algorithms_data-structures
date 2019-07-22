6a939196a0aca503e114ac847f6d9b47c81df14b
Likbes <likbes.js@gmail.com>
2019-07-22 15:57:15 +0300

    complete command pattern

---
+++ patterns/command/index.js
@@ -0,0 +1,70 @@
+"use strict";
+var Command;
+(function (Command) {
+    // concrete commands
+    var SimpleCommand = /** @class */ (function () {
+        function SimpleCommand(payload) {
+            this.payload = payload;
+        }
+        SimpleCommand.prototype.execute = function () {
+            console.log("simple command: print " + this.payload);
+        };
+        return SimpleCommand;
+    }());
+    var ComplexCommand = /** @class */ (function () {
+        function ComplexCommand(receiver, a, b) {
+            this.receiver = receiver;
+            this.a = a;
+            this.b = b;
+        }
+        ComplexCommand.prototype.execute = function () {
+            console.log("complex command:  stuff by receiver object");
+            this.receiver.doSomething(this.a);
+            this.receiver.doSomethingElse(this.b);
+        };
+        return ComplexCommand;
+    }());
+    // important business logic
+    var Receiver = /** @class */ (function () {
+        function Receiver() {
+        }
+        Receiver.prototype.doSomething = function (a) {
+            console.log("Receiver: working with " + a);
+        };
+        Receiver.prototype.doSomethingElse = function (b) {
+            console.log("Receiver: also working with " + b);
+        };
+        return Receiver;
+    }());
+    // send request to command
+    var Invoker = /** @class */ (function () {
+        function Invoker() {
+        }
+        Invoker.prototype.setOnStart = function (command) {
+            this.onStart = command;
+        };
+        Invoker.prototype.setOnFinish = function (command) {
+            this.onFinish = command;
+        };
+        Invoker.prototype.doSomethingImportant = function () {
+            console.log('Invoker: do smth before i begin?');
+            if (this.isCommand(this.onStart)) {
+                this.onStart.execute();
+            }
+            console.log('Invoker: doing smth important');
+            console.log('Invoker: do smth after finish?');
+            if (this.isCommand(this.onFinish)) {
+                this.onFinish.execute();
+            }
+        };
+        Invoker.prototype.isCommand = function (object) {
+            return object.execute !== undefined;
+        };
+        return Invoker;
+    }());
+    var invoker = new Invoker();
+    invoker.setOnStart(new SimpleCommand('Hi!'));
+    var receiver = new Receiver();
+    invoker.setOnFinish(new ComplexCommand(receiver, 'Send email', 'Save report'));
+    invoker.doSomethingImportant();
+})(Command || (Command = {}));
---
+++ patterns/command/index.ts
@@ -0,0 +1,88 @@
+namespace Command {
+  interface Command {
+    execute(): void;
+  }
+
+  // concrete commands
+  class SimpleCommand implements Command {
+    private payload: string;
+
+    constructor(payload: string) {
+      this.payload = payload;
+    }
+
+    public execute(): void {
+      console.log(`simple command: print ${this.payload}`);
+    }
+  }
+
+  class ComplexCommand implements Command {
+    private receiver: Receiver;
+    private a: string;
+    private b: string;
+
+    constructor(receiver: Receiver, a: string, b: string) {
+      this.receiver = receiver;
+      this.a = a;
+      this.b = b;
+    }
+
+    public execute(): void {
+      console.log(`complex command:  stuff by receiver object`);
+      this.receiver.doSomething(this.a);
+      this.receiver.doSomethingElse(this.b);
+    }
+  }
+
+  // important business logic
+  class Receiver {
+    public doSomething(a: string): void {
+      console.log(`Receiver: working with ${a}`);
+    }
+
+    public doSomethingElse(b: string): void {
+      console.log(`Receiver: also working with ${b}`);
+    }
+  }
+
+  // send request to command
+  class Invoker {
+    private onStart?: Command;
+    private onFinish?: Command;
+
+    public setOnStart(command: Command): void {
+      this.onStart = command;
+    }
+
+    public setOnFinish(command: Command): void {
+      this.onFinish = command;
+    }
+
+    public doSomethingImportant(): void {
+      console.log('Invoker: do smth before i begin?');
+      if (this.isCommand(this.onStart)) {
+        this.onStart.execute();
+      }
+
+      console.log('Invoker: doing smth important');
+      console.log('Invoker: do smth after finish?');
+      if (this.isCommand(this.onFinish)) {
+        this.onFinish.execute();
+      }
+    }
+
+    private isCommand(object: any): object is Command {
+      return object.execute !== undefined;
+    }
+  }
+
+  const invoker = new Invoker();
+  invoker.setOnStart(new SimpleCommand('Hi!'));
+
+  const receiver = new Receiver();
+  invoker.setOnFinish(
+    new ComplexCommand(receiver, 'Send email', 'Save report')
+  );
+
+  invoker.doSomethingImportant();
+}
